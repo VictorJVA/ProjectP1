a69540a48a5e2745e31abad6ead7e9316d5380bc
MaximilianoSanLa <maximiliano.sanchez.lara@gmail.com>
2024-03-18 21:33:22 -0400

    datagen

---
+++ datagenerator.py
@@ -0,0 +1,7 @@
+from Client_User.models import Report,File,Appointment,Client,Vet,Log_in, Pets
+for i in range(0,100):
+    appointment = Appointment(i,)
+    report = Report(i,)
+    client = Client(i,)
+    vet= Vet(i,)
+    
