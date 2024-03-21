127654f9bb5eaa55afda64594d6f63460333ef55
JPZU <juanpablozp@icloud.com>
2024-03-21 08:49:07 -0500

    registerPet /complete

--- Client_User/models.py
+++ Client_User/models.py
@@ -4,13 +4,13 @@ from django.db import models
 from django.db import models
 # Create your models here.
 class Client(models.Model):
-    client_id= models.IntegerField(max_length=None,primary_key=True)
+    client_id= models.AutoField(max_length=None,primary_key=True)
     log_id= models.ForeignKey('Log_in',on_delete=models.CASCADE,null=False)
     name= models.CharField(max_length=50)
     image= models.ImageField(upload_to='client/images/')
 
 class Pets(models.Model):
-    pet_id= models.IntegerField(max_length= None,primary_key=True)
+    pet_id= models.AutoField(max_length= None,primary_key=True)
     client_id= models.ForeignKey('Client',on_delete=models.CASCADE,null=False)
     name=models.CharField(max_length=100, null=False)
     species=models.CharField(max_length=100, null=False)
@@ -21,7 +21,7 @@ class Pets(models.Model):
     image= models.ImageField(upload_to='pet/images/')
     
 class Medical_history(models.Model):
-    Medical_history_id= models.IntegerField(max_length=None,primary_key=True)
+    Medical_history_id= models.AutoField(max_length=None,primary_key=True)
     pet_id=models.ForeignKey('Pets',on_delete=models.CASCADE,null=False)
     
 class Phone_Owner(models.Model):
@@ -30,24 +30,24 @@ class Phone_Owner(models.Model):
     phone=models.CharField(max_length=15)
     
 class Vaccination(models.Model):
-    vaccination_id=models.IntegerField(max_length=None,primary_key=True)
+    vaccination_id=models.AutoField(max_length=None,primary_key=True)
     medical_history_id=models.ForeignKey('Medical_history',on_delete=models.CASCADE,null=False)
     vaccine=models.ForeignKey('Vaccines',on_delete=models.CASCADE,null=False)
     vaccine_date= models.DateField(auto_now=False,auto_now_add=False,null=True)
 
 class Vaccines(models.Model):
-    vaccine_id=models.IntegerField(max_length=None,primary_key=True)
+    vaccine_id=models.AutoField(max_length=None,primary_key=True)
     vaccine_name= models.CharField(max_length=100, null=True)
 
 
 #QUICK SEPARATION
 class Log_in(models.Model):
-    log_id=models.IntegerField(max_length=None,primary_key=True)
+    log_id=models.AutoField(max_length=None,primary_key=True)
     user_name=models.CharField(max_length=255,unique=True)
     password= models.CharField(max_length=255)
 
 class Vet(models.Model):
-    vet_id=models.IntegerField(max_length=None,primary_key=True)
+    vet_id=models.AutoField(max_length=None,primary_key=True)
     log_id=models.ForeignKey('Log_in',on_delete=models.CASCADE,null=False)
     name= models.CharField(max_length=50)
     country_intials=models.CharField(max_length=4)
@@ -57,7 +57,7 @@ class Vet(models.Model):
 
 
 class Appointment(models.Model):
-    appointment_id=models.IntegerField(max_length=None,primary_key=True)
+    appointment_id=models.AutoField(max_length=None,primary_key=True)
     pet_id= models.ForeignKey('Pets',on_delete=models.CASCADE,null=False)
     vet_id= models.ForeignKey('Vet',on_delete=models.CASCADE,null=False)
     date=models.DateField()
@@ -65,7 +65,7 @@ class Appointment(models.Model):
     reason_appointment=models.CharField(max_length=200,null=True)
 
 class Report(models.Model):
-    report_id=models.IntegerField(max_length=None,primary_key=True)
+    report_id=models.AutoField(max_length=None,primary_key=True)
     appointement_id=models.ForeignKey('Appointment',on_delete=models.CASCADE,null=False)
     medical_history_id=models.ForeignKey('Medical_history',on_delete=models.CASCADE,null=False)
     test_findings=models.CharField(max_length=200, null=True)
--- client/views.py
+++ client/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from Client_User.models import Report,File,Appointment,Client,Vet,Log_in, Pets
 from django.shortcuts import render, redirect
+from .forms import *
 import pdb
 
 def homeClient(request, id_cliente):
 
     client = Client.objects.get(pk=id_cliente)
     pets = Pets.objects.filter(client_id=client)
-    print(pets)
+    # print(pets)
     # pdb.set_trace()
     return render(request, 'homeClient.html', {'pets': pets, 'client': client})
 
-def registerPet(request):
-    return render(request, 'registerPet.html')
+def registerPet(request, id_cliente):
+    client = Client.objects.get(pk=id_cliente)
+    form = PetForm(request.POST, request.FILES)
+    if form.is_valid():
+        pet = form.save(commit=False)  # Guarda el formulario pero no lo inserta en la base de datos todavía
+        pet.client_id = client  # Asigna el cliente a la mascota
+        pet.save()  # Ahora guarda la mascota en la base de datos junto con el cliente
+        return redirect(f'/homeClient/{client.client_id}')
+
+
+    context = {
+        'client': client,
+        'form': form,
+    }
+    
+    return render(request, 'registerPet.html', context)
 
 def rateVet(request):
     return render(request, 'rateVet.html')
