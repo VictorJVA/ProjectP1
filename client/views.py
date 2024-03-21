7e73d55efb6a2a48335610fce8c8150e803c5933
MaximilianoSanLa <maximiliano.sanchez.lara@gmail.com>
2024-03-21 11:06:38 -0400

    Modified views

--- client/views.py
+++ client/views.py
@@ -11,7 +11,7 @@ def homeClient(request, id_cliente):
     pets = Pets.objects.filter(client_id=client)
     # print(pets)
     # pdb.set_trace()
-    return render(request, 'homeClient.html', {'pets': pets, 'client': client})
+    return render(request, 'homeClient.html', {'pets': pets, 'client': client}, {'pets': pets, 'client': client})
 
 def registerPet(request, id_cliente):
     client = Client.objects.get(pk=id_cliente)
@@ -30,5 +30,19 @@ def registerPet(request, id_cliente):
     
     return render(request, 'registerPet.html', context)
 
-def rateVet(request):
-    return render(request, 'rateVet.html')
+def rateVet(request, id_client, vet_id):
+    client=Client.objects.get(pk=id_client)
+    vet_id= Vet.objects.get(pk=vet_id)
+    appointment= Appointment.objects.filter(client_id=client).filter(vet_id=vet_id)
+    if appointment and request.GET.get('rating')!=None:
+        appointment.update(comment=request.GET.get('comment'))
+        appointment.update(rating=request.GET.get('rating'))
+        appointment=appointment[0]
+    else:
+        appointment=appointment.get(vet_id=vet_id)
+    print(client.client_id)
+    print(client)
+    return render(request, 'rateVet.html',{'appointment':appointment,'client':client,'rate':appointment.rating,'com':appointment.comment})
+def appointmentViewClient(request, id_pet):
+    appointment= Appointment.objects.filter(pet_id=id_pet)
+    return render(request,'appointmentViewClient.html',{'appointment':appointment})
