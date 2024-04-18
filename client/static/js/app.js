3220c3d70cb3d89a7d85e21baadfd1f117a792e6
MaximilianoSanLa <maximiliano.sanchez.lara@gmail.com>
2024-04-17 23:55:48 -0400

    Appointment Accept

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -1,14 +1,10 @@
+// Definir la función en el ámbito global
+window.abrir_modal_crear = function(url) {
+  $('#crear').load(url,function(){
+    $(this).modal('show');
+  });
+};
 
-
-
-
-// // Definir la función en el ámbito global
-// window.abrir_modal_crear = function(url) {
-//   $('#crear').load(url,function(){
-//     $(this).modal('show');
-//   });
-// };
-
-// jQuery(document).ready(function($) {
-//   // Otras operaciones que puedas tener dentro de tu función jQuery(document).ready()
-// });
+jQuery(document).ready(function($) {
+  // Otras operaciones que puedas tener dentro de tu función jQuery(document).ready()
+});
--- web_project/urls.py
+++ web_project/urls.py
@@ -49,7 +49,7 @@ urlpatterns = [
     path('clinicalUserView/<int:user_id>/<int:pet_id>/',user.clinicalUserView,name= 'clinicalUserView' ),
     path('viewRate/<int:client_id>/',client.viewRate,name='viewRate'),
     path('statistics/',views.stastistics,name="statistics"),
-    path('appointmentAccept/<int:user_id>/',user.appointmentAccept,name='appointmentAccept'),
+    path('appointmentAccept/<int:user_id>/<int:appointment_id>/',user.appointmentAccept,name='appointmentAccept'),
     path('update-field/', user.update_field, name='update-field'),
 ]
 
